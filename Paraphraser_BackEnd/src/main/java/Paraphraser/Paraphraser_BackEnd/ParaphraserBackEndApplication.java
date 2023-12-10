7715df08b036c7cd4ee397db7ab41bc1f0674c6c
edmundchan70 <edmundchan70@gmail.com>
2023-12-10 15:36:21 -0500

    spring boot init

---
+++ Paraphraser_BackEnd/src/main/java/Paraphraser/Paraphraser_BackEnd/ParaphraserBackEndApplication.java
@@ -0,0 +1,13 @@
+package Paraphraser.Paraphraser_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaphraserBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaphraserBackEndApplication.class, args);
+	}
+
+}
---
+++ Paraphraser_BackEnd/src/test/java/Paraphraser/Paraphraser_BackEnd/ParaphraserBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Paraphraser.Paraphraser_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaphraserBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
